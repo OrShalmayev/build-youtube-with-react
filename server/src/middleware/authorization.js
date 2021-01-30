e5ade7ec12699e166286db8e9c5ed2fc57c50f56
Or Shalmayev <shalmayev1993@gmail.com>
2021-01-30 23:18:21 +0200

    initial commit

--- server/src/middleware/authorization.js
+++ server/src/middleware/authorization.js
@@ -1,3 +1,58 @@
-export async function getAuthUser(req, res, next) {}
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
+import jwt from 'jsonwebtoken'
+export async function getAuthUser(req, res, next) {
+    if(!req.headers.authorization) {
+        req.user = null;
+        return next();
+    }
 
-export async function protect(req, res, next) {}
+    const token = req.headers.authorization
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: decoded.id
+        },
+        include: {
+            videos: true
+        }
+    });
+
+    req.user = user;
+    next();
+}
+
+export async function protect(req, res, next) {
+    if(!req.headers.authorization) {
+        console.log(req.headers)
+
+        return next({
+            message: 'you need to be logged in to visit this route',
+            statusCode: 401
+        })
+    }
+
+    try {
+        const token = req.headers.authorization
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: decoded.id
+            },
+            include: {
+                videos: true
+            }
+        });
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error)
+        next({
+            message: 'you need to be logged in to visit this route',
+            statusCode: 401
+        })
+    }
+}
